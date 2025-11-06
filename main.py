b57ba21cb0683753c4b17292efd6f1e0bdebe580
muriha-code <muhamadrifkyh09@gmail.com>
2025-11-06 09:02:23 +0700

    update file main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("file main")
+from modules import login, barang, transaksi, laporan
+
+def menu_admin():
+    while True:
+        print("""
+=== MENU ADMIN ===
+1. Lihat Barang
+2. Tambah Barang
+3. Ubah Barang
+4. Hapus Barang
+5. Transaksi Penjualan
+6. Laporan Penjualan
+7. Keluar
+""")
+        pilih = input("Pilih menu: ")
+        if pilih == "1":
+            barang.tampilkan_barang()
+        elif pilih == "2":
+            barang.tambah_barang()
+        elif pilih == "3":
+            barang.ubah_barang()
+        elif pilih == "4":
+            barang.hapus_barang()
+        elif pilih == "5":
+            transaksi.transaksi_penjualan()
+        elif pilih == "6":
+            laporan.laporan_penjualan()
+        elif pilih == "7":
+            print("Keluar dari program...")
+            break
+        else:
+            print("Pilihan tidak valid!")
+
+def main():
+    role = login.login()
+    if role:
+        menu_admin()
+    else:
+        print("Akses ditolak!")
+
+if __name__ == "__main__":
+    main()
